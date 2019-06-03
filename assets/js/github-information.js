57ee8d94abf2448e39674d61d98244bdf19b5519
Kitty McDonagh <kittymcdonagh@gmail.com>
2019-06-03 12:24:07 +0000

    repo list displayed

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,8 +1,3 @@
-// This is the function that is called when the promise (when) is resolved
-// It takes one argument, user, which is the object returned by github api - See https://developer.github.cim/v3/users/
-// The object has many methods such as, user's name, login name, and links to their profile
-// These will be returned in a formatted html string
-
 function userInformationHTML(user) {
     return `
         <h2>${user.name}
@@ -20,6 +15,27 @@ function userInformationHTML(user) {
         </div>`;
 }
 
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>  // whn you click on repo name open a blank page
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+}
+
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
@@ -34,11 +50,14 @@ function fetchGitHubInformation(event) {
         </div>`);
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
-            var userData = response;
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
         function(errorResponse) {
             if (errorResponse.status === 404) {
